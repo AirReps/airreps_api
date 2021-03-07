c694dcc916d7b16c8a5b29adb39a2337f1546b5c
wavpro <31097540+wavpro@users.noreply.github.com>
2021-03-07 13:11:40 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch');
 module.exports = {
     members: async function() {
         return new Promise(async function(resolve, reject){
-            fetch('https://KindImpoliteExperiments.albinhedwall.repl.co/members')
+            fetch('https://api.airreps.info/members')
             .catch(err => reject(new Error(err)))
             .then(res => res.json())
             .then(json => resolve(json));
@@ -13,10 +13,32 @@ module.exports = {
     },
     chipdoc: async function(chipset) {
         return new Promise(async function(resolve, reject){
-            fetch(`https://KindImpoliteExperiments.albinhedwall.repl.co/chipdoc?chip=${chipset}`)
+            fetch(`https://api.airreps.info/chipdoc?chip=${chipset}`)
             .catch(err => reject(new Error(err)))
             .then(res => res.json())
             .then(json => resolve(json));
         })
+    },
+    readme: async function() {
+        return "https://github.com/AirReps/airreps_api";
+    },
+    advanced: {
+        chipdoc: async function(chipset, link, auth) {
+            return new Promise((resolve, reject) => {
+                let requestData = {
+                    chip: chipset,
+                    link: link,
+                    auth: auth
+                }
+                fetch('https://api.airreps.info/chipdoc', { 
+                    method: 'POST', 
+                    body: JSON.stringify(requestData),
+                    headers: { 'Content-Type': 'application/json' } 
+                })
+                .catch(err => reject(new Error(err)))
+                .then(res => res.json()) // expecting a json response
+                .then(json => resolve(json));
+            })
+        }
     }
 }
